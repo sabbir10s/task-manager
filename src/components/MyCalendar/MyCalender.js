f6e4d6dfbf0efbc16590534646d555754f591c49
sabbir10s <sabbirahmed1023@gmail.com>
2022-07-01 10:45:41 +0600

    calender added

--- src/components/MyCalendar/MyCalender.js
+++ src/components/MyCalendar/MyCalender.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const MyCalender = () => {
+    const [value, onChange] = useState(new Date());
+
     return (
-        <div>
-            <h1> Calendar</h1>
+        <div className='flex justify-center'>
+            <div className='py-10 lg:py-32'>
+                <Calendar onChange={onChange} value={value} />
+            </div>
         </div>
     );
 };
